e316ccc8b4caae501be8d2d82e27caa3c8009ca0
jruminski <j.ruminski@gmail.com>
2024-06-03 20:46:43 +0200

    fix mvn tests

--- src/test/java/wavestone/automotive/parts/CarPartsApplicationTests.java
+++ src/test/java/wavestone/automotive/parts/CarPartsApplicationTests.java
@@ -2,8 +2,10 @@ package wavestone.automotive.parts;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestCarPartsApplication.class)
 class CarPartsApplicationTests {
 
 	@Test
